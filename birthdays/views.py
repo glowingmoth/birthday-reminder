ad0f6a4faea8c40d83147f9864712f5eaef3b307
Thomas Doig <thomasdoig@hotmail.com>
2021-06-29 22:16:00 +1000

    added a login decorator for user authorisation to home page

--- birthdays/views.py
+++ birthdays/views.py
@@ -1,8 +1,9 @@
 from .models import Birthday
 from django.utils import timezone
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-
+@login_required(login_url='login')
 def index(request):
   timeNow = timezone.now()
   context = {
