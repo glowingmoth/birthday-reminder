3c55304a27231e978ba976a7f889016d39ac14d6
Thomas Doig <thomasdoig@hotmail.com>
2021-06-30 11:06:36 +1000

    Added a brithday list for the most recent week

--- birthdays/views.py
+++ birthdays/views.py
@@ -7,11 +7,15 @@ from django.contrib.auth.decorators import login_required
 def index(request):
   timeNow = timezone.now()
   context = {
+    'timeNow': timeNow,
     'object_list': Birthday.objects.all(),
     'object_this_month': Birthday.objects.filter(birthDate__month=timeNow.strftime('%m')),
-    'timeNow': timeNow,
+    'object_this_week': Birthday.objects.filter(birthDate__month=timeNow.strftime('%m'), birthDate__week=timeNow.strftime('%W')),
     'object_today': Birthday.objects.filter(birthDate__month=timeNow.strftime('%m'), birthDate__day=timeNow.strftime('%d'))
   }
   return render(request, 'birthdays/index.html', context)
 
+
+# The Query Set field lookup '__day' only takes integers however .strftime() 2nd argument '%d' uses decimals which caused a bug
+# when displaying birthdays for the current day. Converting the formatted number to an integer fixed the bug. 
   
--- main/settings.py
+++ main/settings.py
@@ -99,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Australia/Brisbane'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
