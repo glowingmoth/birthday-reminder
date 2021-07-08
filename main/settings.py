33d34cdbfce0af8a9d476d78a64a946f9a343aa4
Thomas Doig <thomasdoig@hotmail.com>
2021-07-08 16:24:10 +1000

    removed some comments from settings.py

--- main/settings.py
+++ main/settings.py
@@ -8,8 +8,6 @@ environ.Env.read_env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
@@ -26,7 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessionSTATIC_ROOT = os.path.join(BASE_DIR, ‘staticfiles’)s',
+    'django.contrib.session',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'birthdays',
