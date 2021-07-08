e3650515ce4470a50b40894dc8c0f16ffc92d0ab
Thomas Doig <thomasdoig@hotmail.com>
2021-07-08 15:42:37 +1000

    installed gunicorn

--- main/settings.py
+++ main/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['django-birthday-reminder.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', 'django-birthday-reminder.herokuapp.com']
 
 
 # Application definition
@@ -114,7 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR, 'staticfiles'
+
 STATIC_URL = '/static/'
 
 # Default primary key field type
